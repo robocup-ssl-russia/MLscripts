25ab72ca16acbfba5f1f4c2a81bd8eea5b2b402e
Dina Funt <dina.funt@gmail.com>
2018-10-20 21:38:22 +0300

    fixed code style

--- algorithms/MoveToPoint.m
+++ algorithms/MoveToPoint.m
@@ -2,22 +2,25 @@
 %Stop in the vicinity of the ball and turn to it.
 
 function rul = MoveToPoint(agent, G) 
-angF = atan2((G(2) - agent.y), (G(1) - agent.x));
-angR = agent.ang;
-dif = angF - angR; 
+angFinal = atan2((G(2) - agent.y), (G(1) - agent.x));
+angRobot = agent.ang;
+dif = angFinal - angRobot;
 
-dist = sqrt((agent.x - G(1)) ^2 + (agent.y - G(2))^2);
+coef = 60 / 650; %linear velocity coefficient
+coef1 = 15;      %angular velocity coefficient
 
-SpeedX = 60 * cos(pi / 2 + dif) * (dist / 650);
-SpeedY = 60 * sin(pi / 2 + dif) * (dist / 650);
+dist = sqrt((agent.x - G(1)) ^ 2 + (agent.y - G(2)) ^ 2);
+
+SpeedX = coef * cos(pi / 2 + dif) * dist;
+SpeedY = coef * sin(pi / 2 + dif) * dist;
 
 r = 100;    %radius of the robot
-err = 50;   %the radius of the vicinity of G
-vicinity = r + err; 
+err = 50;   %radius of the vicinity of G
+vicinity = r + err;
 
-if (abs(agent.x - G(1)) > vicinity || abs(agent.y - G(2)) > vicinity)
+if (dist > vicinity)
      rul = Crul(SpeedX, SpeedY, 0, 0, 0);
 else
-    rul = Crul(0, 0, 0, dif * 15, 0);
+    rul = Crul(0, 0, 0, dif * coef1, 0);
 end
 end
